8e10471d77402a61c005fcca54cb0e177d42482b
Jackson Reynolds <jwreynold@gmail.com>
2025-04-12 11:06:08 -0400

    implement db, add fuzzy-ish text search

---
+++ DISCUSSION.md
@@ -0,0 +1,15 @@
+Things to Implement: 
+
+Client:
+- [x] Fetch filtered results when search term changes
+- [ ] component library
+- [ ] infinite scroll? just page it if too intense, though could probably be sovled with a correct component library choice
+- [ ] abstract api?
+- [ ] loading state
+
+API:
+- [x] take query parameters and filter results
+
+DB:
+- [x] fuzzy/full text search
+- [ ] text search index(es?)
--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,33 @@
+import { sql } from "drizzle-orm";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+const toTsQueryArg = (query: string) =>
+  query
+    .trim()
+    .split(/\s+/)
+    .map((term) => `${term}:*`)
+    .join(" & ");
 
-  const data = advocateData;
+const fuzzySearchQuery = (searchTerm: string) => sql`(
+  setweight(to_tsvector('english', ${advocates.firstName}), 'A') ||
+  setweight(to_tsvector('english', ${advocates.lastName}), 'A') ||
+  setweight(to_tsvector('english', ${advocates.city}), 'A') ||
+  setweight(to_tsvector('english', ${advocates.degree}), 'A') ||
+  setweight(to_tsvector('english', cast(${advocates.yearsOfExperience} as text)), 'A') ||
+  setweight(to_tsvector('english', cast(${advocates.specialties} as text)), 'A') ||
+  setweight(to_tsvector('english', cast(${advocates.phoneNumber} as text)), 'A')
+    @@ to_tsquery('english', ${searchTerm})
+  )`;
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const searchInput = searchParams.get("search") ?? "";
+  const searchTerm = toTsQueryArg(searchInput);
+
+  const data = searchInput
+    ? await db.select().from(advocates).where(fuzzySearchQuery(searchTerm))
+    : await db.select().from(advocates);
 
   return Response.json({ data });
 }
